45712d4ec7f606ad3950145ba4d020af351bda69
Aaron <m17857255506@163.com>
2022-03-09 15:57:02 +0800

    test ok

--- softwares_install/internet/submodules/chrome.sh
+++ softwares_install/internet/submodules/chrome.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 install_path=~/Documents/installation
-https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 chrome_name=google-chrome-stable_current_amd64.deb
 google-chrome --version
 if [ $? -ne 0 ]; then
-    wget https://release.axocdn.com/linux/${chrome_name} -P ${install_path}
-    if [ $? -ne 0 ]; then
+    wget https://dl.google.com/linux/direct/${chrome_name} -P ${install_path}
+    if [ $? -eq 0 ]; then
         sudo dpkg -i ${install_path}/${chrome_name}
     fi
 else
--- softwares_install/internet/submodules/vscode.sh
+++ softwares_install/internet/submodules/vscode.sh
@@ -4,7 +4,7 @@ vscode_name=code_1.65.1-1646706496_amd64.deb
 code --version
 if [ $? -ne 0 ]; then
     wget http://vscode.cdn.azure.cn/stable/8908a9ca0f221f36507231afb39d2d8d1e182702/${vscode_name} -P ${install_path}
-    if [ $? -ne 0 ]; then
+    if [ $? -eq 0 ]; then
         sudo dpkg -i ${install_path}/${vscode_name}
     fi
 else
