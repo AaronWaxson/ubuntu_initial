561acc7b7e01a7d9f238c46ac8de4d21f578a656
Aaron <m17857255506@163.com>
2022-03-09 16:10:17 +0800

    fix a bug and miniconda test ok

--- softwares_install/internet/submodules/miniconda.sh
+++ softwares_install/internet/submodules/miniconda.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 install_path=~/Documents/installation
 conda_name=Miniconda3-latest-Linux-x86_64.sh
-if [ ! -d ${install_path}/${conda_name} ]; then
+if [ ! -f ${install_path}/${conda_name} ]; then
     wget https://repo.anaconda.com/miniconda/${conda_name} -P ${install_path}
 fi
 
-echo "${conda_name} downloaded, need to install manually"
+bash ${install_path}/${conda_name} -b -p $HOME/miniconda
+echo "${conda_name} installed with silent mode."
--- softwares_install/internet/submodules/zetoro.sh
+++ softwares_install/internet/submodules/zetoro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 compress_path=~/Documents/compress
 zetoro_name=Zotero-5.0.96.3_linux-x86_64.tar.bz2
-if [ ! -d ${compress_path}/${zetoro_name} ]; then
+if [ ! -f ${compress_path}/${zetoro_name} ]; then
     wget https://download.zotero.org/client/release/5.0.96.3/${zetoro_name} -P ${compress_path}
 fi
 # To-do: plugins
