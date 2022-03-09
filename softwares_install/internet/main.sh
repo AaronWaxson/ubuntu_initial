b03af2bd843bd9c3333e78ff060092942358118e
Aaron <m17857255506@163.com>
2022-03-09 15:06:22 +0800

    add miniconda3

--- softwares_install/internet/main.sh
+++ softwares_install/internet/main.sh
@@ -41,6 +41,10 @@ fi
 # To-do: plugins
 
 # miniconda
+conda_name=Miniconda3-latest-Linux-x86_64.sh
+if [ ! -d ${install_path}/${conda_name} ]; then
+    wget https://repo.anaconda.com/miniconda/${conda_name} -P ${install_path}
+fi
 
 # chrome
 
