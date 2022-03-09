38d8b1db645d3c0e9c511473d16f33b7afd1b9dc
Aaron <m17857255506@163.com>
2022-03-09 20:20:34 +0800

    add exist check

--- softwares_install/internet/submodules/joplin/joplin.sh
+++ softwares_install/internet/submodules/joplin/joplin.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# softwares_path=~/Documents/softwares
-# joplin_name=Joplin-2.7.13.AppImage
-# if [ ! -f ${softwares_path}/${joplin_name} ]; then
-#     wget https://github-do.panbaidu.cn//https://github.com/laurent22/joplin/releases/download/v2.7.13/${joplin_name} -P ${softwares_path}
-# fi
-# echo "-${joplin_name} downloaded!"
-
-. ./install.sh
+joplin_path=$HOME/.joplin/Joplin.AppImage
+if [ ! -f ${joplin_path} ]; then
+    . ./install.sh
+fi
+echo "joplin installed."
