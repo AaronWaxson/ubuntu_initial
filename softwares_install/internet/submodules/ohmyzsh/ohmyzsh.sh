ac87bc6163be78fb374788b0c9aab0ac86e081fc
Aaron <m17857255506@163.com>
2022-03-09 21:01:42 +0800

    oh my zsh test ok

--- softwares_install/internet/submodules/ohmyzsh/ohmyzsh.sh
+++ softwares_install/internet/submodules/ohmyzsh/ohmyzsh.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-zsh
+zsh --help
 if [ $? -ne 0 ]; then
     sh -c ./install.sh
 fi
 echo "oh_my_zsh installed."
 
 # plugins
+# autosuggestion
+git clone https://github.do/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# highlighting
+git clone https://github.do/https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # powerlevel9k theme
 git clone https://github.do/https://github.com/bhilburn/powerlevel9k.git ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k
 
 # nerd-fonts
 font_path=${HOME}/Downloads
-if [ ! -d ${font_path}/Hack\ Regular\ Nerd\ Font\ Complete.ttf ]; then
+if [ ! -f ${font_path}/Hack\ Regular\ Nerd\ Font\ Complete.ttf ]; then
     wget https://github-do.panbaidu.cn//https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf -P ${font_path}
 fi
 echo "nerd-fonts downloaded, but need manually install and configure."
