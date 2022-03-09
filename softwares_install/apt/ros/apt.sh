335181c1491487a873a1c1387e3ca2ba401ec245
Aaron <m17857255506@163.com>
2022-03-09 23:00:09 +0800

    ros ok

--- softwares_install/apt/ros/apt.sh
+++ softwares_install/apt/ros/apt.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ `lsb_release -cs` main" > /etc/apt/sources.list.d/ros-latest.list'
 
-# download rosdistro
 distro_path=${HOME}/Documents/rosdistro
-if [ ! -d ${distro_path} ]
-then
+ros_distro=noetic
+# download rosdistro
+if [ ! -d ${distro_path} ]; then
     git clone https://github.do/https://github.com/ros/rosdistro.git ${distro_path}
 fi
 sudo apt install curl -y # if you haven't already installed curl
 sudo apt-key add ${distro_path}/ros.asc
 
-sudo apt update & sudo apt install ros-noetic-desktop-full -y
-if [ $? -ne 0 ]
-then
-    source /opt/ros/noetic/setup.bash
+sudo apt update &
+sudo apt install ros-${ros_distro}-desktop-full -y
+
+if [ -n "$ZSH_VERSION" ]; then
+   # assume zsh    
+   source /opt/ros/${ros_distro}/setup.zsh
+else
+    source /opt/ros/${ros_distro}/setup.bash
 fi
 
-if [ $? -ne 0 ]
-then
-    sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
-    if [ $? -ne 0 ]
-    then
-        sudo apt install python3-rosdep -y
-    fi
-fi
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+sudo apt install python3-rosdep -y
--- softwares_install/apt/ros/rosdep.sh
+++ softwares_install/apt/ros/rosdep.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 distro_path=${HOME}/Documents/rosdistro
+change_path=https://raw.githubusercontent.com/ros/rosdistro/master
+target_path=file://${distro_path}
 
 # sources.list.d 
-change_path=https://raw.githubusercontent.com/ros/rosdistro
-target_path=file://${distro_path}
 sed -i "s|${change_path}|${target_path}|g" ${distro_path}/rosdep/sources.list.d/20-default.list
 
-#sudo rosdep init
-#rosdep update
-#rm -rf ${distro_path}
+# rosdep2
+sudo sed -i "s|${change_path}|${target_path}|g" /usr/lib/python3/dist-packages/rosdep2/sources_list.py
+
+# rep3
+sudo sed -i "s|${change_path}|${target_path}|g" /usr/lib/python3/dist-packages/rosdep2/rep3.py
+
+# __init__
+sudo sed -i "s|${change_path}|${target_path}|g" /usr/lib/python3/dist-packages/rosdistro/__init__.py
+
+sudo rosdep init
+rosdep update
+
+rm -rf ${distro_path}
