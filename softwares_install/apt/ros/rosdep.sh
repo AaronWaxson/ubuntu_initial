4f40fce8cd522616fff40d8f121368698a6e3dee
Aaron <m17857255506@163.com>
2022-03-09 14:40:08 +0800

    add rosdep

---
+++ softwares_install/apt/ros/rosdep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+distro_path=~/Documents/rosdistro
+
+# sources.list.d 
+change_path=https://raw.githubusercontent.com/ros/rosdistro
+target_path=file://${distro_path}
+sed -i "s|${change_path}|${target_path}|g" ${distro_path}/rosdep/sources.list.d/20-default.list
+
+#sudo rosdep init
+#rosdep update
+#rm -rf ${distro_path}
