2fa2fc1cac3e262aa64040e3c95caee7e26cbf7d
aaron <m17857255506@163.com>
2022-03-11 22:03:56 +0800

    change dir name

--- softwares/internet/submodules/miniconda/install.sh
+++ softwares/internet/submodules/miniconda/install.sh
@@ -4,7 +4,7 @@ conda_name=Miniconda3-latest-Linux-x86_64.sh
 if [ ! -f ${install_path}/${conda_name} ]; then
     wget https://repo.anaconda.com/miniconda/${conda_name} -P ${install_path}
     if [ $? -eq 0 ]; then
-        bash ${install_path}/${conda_name} -b -p ${HOME}/miniconda
+        bash ${install_path}/${conda_name} -b -p ${HOME}/miniconda3
     fi
 fi
 
