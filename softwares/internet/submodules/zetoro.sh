839a9f8f261744488d7de0093c8a3344f5258f59
aaron <m17857255506@163.com>
2022-03-11 22:03:33 +0800

    fix a '&' bug

--- softwares/internet/submodules/zetoro.sh
+++ softwares/internet/submodules/zetoro.sh
@@ -7,7 +7,7 @@ if [ $? -ne 0 ]; then
     sudo apt install zotero -y
 
     # juris-m
-    wget -qO- https://apt.retorque.re/file/zotero-apt/install.sh | sudo bash
-    sudo apt update
+    wget -qO- https://apt.retorque.re/file/zotero-apt/install.sh | sudo bash &&
+    sudo apt update &&
     sudo apt install jurism -y
 fi
