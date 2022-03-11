e6819558e5d2dec98ccbeae6f0bcadaf461e6cae
aaron <m17857255506@163.com>
2022-03-11 22:05:46 +0800

    refuse useless command

--- softwares/apt/submodules/docker/sources_change.sh
+++ softwares/apt/submodules/docker/sources_change.sh
@@ -3,7 +3,6 @@ source_path=$(pwd)/softwares/apt/submodules/docker/daemon.json
 target_path=/etc/dockerdaemon.json
 if [ ! -f ${target_path} ]; then
     sudo cp ${source_path} ${target_path}
+    systemctl daemon-reload
+	systemctl restart docker
 fi
-
-systemctl daemon-reload
-systemctl restart docker
