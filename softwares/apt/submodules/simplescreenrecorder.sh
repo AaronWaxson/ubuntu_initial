f0fa267bc486bf6e5c6999091c11322ec0f9986e
aaron <m17857255506@163.com>
2022-03-12 13:03:58 +0800

    add installed check

--- softwares/apt/submodules/simplescreenrecorder.sh
+++ softwares/apt/submodules/simplescreenrecorder.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
-sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder -y &&
-    sudo apt-get update && sudo apt-get install simplescreenrecorder -y
+
+if [ ! -f /usr/bin/simplescreenrecorder ]; then
+    sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder -y &&
+        sudo apt-get update && sudo apt-get install simplescreenrecorder -y
+fi
--- softwares/apt/submodules/system_monitor.sh
+++ softwares/apt/submodules/system_monitor.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
-sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor -y &&
-    sudo apt-get update && sudo apt-get install indicator-sysmonitor -y
+
+if [ ! -f /usr/bin/indicator-sysmonitor ]; then
+    sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor -y &&
+        sudo apt-get update && sudo apt-get install indicator-sysmonitor -y
+fi
