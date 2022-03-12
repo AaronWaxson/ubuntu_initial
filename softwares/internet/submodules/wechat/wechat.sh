59a5b38bd9db520699ce2668befed5a7867d3ade
aaron <m17857255506@163.com>
2022-03-12 10:01:49 +0800

    add installed check

--- softwares/internet/submodules/wechat/wechat.sh
+++ softwares/internet/submodules/wechat/wechat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo docker pull bestwu/wechat
-if [ $? -eq 0 ]; then
-    source $(pwd)/softwares/internet/submodules/wechat/run.sh
+docker image ls | grep wechat
+if [ $? -ne 0 ]; then
+    sudo docker pull bestwu/wechat
+    if [ $? -eq 0 ]; then
+        source $(pwd)/softwares/internet/submodules/wechat/run.sh
+    fi
 fi
-
