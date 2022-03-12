b053e7ba8801267c8b69a79d149da1e3ff1f0f8f
aaron <m17857255506@163.com>
2022-03-12 11:05:26 +0800

    suppress  log msgs

--- sources/apt_change.sh
+++ sources/apt_change.sh
@@ -2,7 +2,7 @@
 set -e
 apt_path=/etc/apt/sources.list
 source_name=mirrors.aliyun.com
-cat ${apt_path} | grep ${source_name}
+cat ${apt_path} | grep ${source_name} > /tmp/apt_change
 if [ $? -eq 0 ]; then
     sudo cp ${apt_path} ${apt_path}.bak
     # change inland sources
