cf14ea6bab7573300e6de969bb49bbb547db5666
aaron <m17857255506@163.com>
2022-03-12 10:39:25 +0800

    add rosdep check

--- softwares/apt/submodules/ros/rosdep.sh
+++ softwares/apt/submodules/ros/rosdep.sh
@@ -4,7 +4,7 @@ distro_path=${HOME}/Documents/rosdistro
 change_path=https://raw.githubusercontent.com/ros/rosdistro/master
 target_path=file://${distro_path}
 
-# sources.list.d 
+# sources.list.d
 sed -i "s|${change_path}|${target_path}|g" ${distro_path}/rosdep/sources.list.d/20-default.list
 
 # rosdep2
@@ -16,7 +16,9 @@ sudo sed -i "s|${change_path}|${target_path}|g" /usr/lib/python3/dist-packages/r
 # __init__
 sudo sed -i "s|${change_path}|${target_path}|g" /usr/lib/python3/dist-packages/rosdistro/__init__.py
 
-sudo rosdep init
-rosdep update
-
+source_list_path=/etc/ros/rosdep/sources.list.d/20-default.list
+if [ ! -f ${source_list_path} ]; then
+    sudo rosdep init
+    rosdep update
+fi
 rm -rf ${distro_path}
