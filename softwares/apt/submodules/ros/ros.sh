5b82195dc2a49247300812d06d853a4bf1e156aa
aaron <m17857255506@163.com>
2022-03-12 13:20:47 +0800

    update check path

--- softwares/apt/submodules/ros/ros.sh
+++ softwares/apt/submodules/ros/ros.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-source_list_path=/etc/ros/rosdep/sources.list.d/20-default.list
-if [ ! -f ${source_list_path} ]; then
-    . $(pwd)/softwares/apt/submodules/ros/apt.sh &&
-        . $(pwd)/softwares/apt/submodules/ros/rosdep.sh
+check_path=/opt/ros/noetic/bin/roscore
+if [ ! -f ${check_path} ]; then
+    . $(pwd)/softwares/apt/submodules/ros/apt.sh
+    . $(pwd)/softwares/apt/submodules/ros/rosdep.sh
 fi
