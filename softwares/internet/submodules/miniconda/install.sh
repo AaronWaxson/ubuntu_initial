6359307af00f2df086583765763eb4f1ece797a1
aaron <m17857255506@163.com>
2022-03-12 13:03:22 +0800

    change miniconda check

--- softwares/internet/submodules/miniconda/install.sh
+++ softwares/internet/submodules/miniconda/install.sh
@@ -3,11 +3,9 @@ set -e
 
 install_path=${HOME}/Documents/installation
 conda_name=Miniconda3-latest-Linux-x86_64.sh
-if [ ! -f ${install_path}/${conda_name} ]; then
-    wget https://repo.anaconda.com/miniconda/${conda_name} -P ${install_path}
-    if [ $? -eq 0 ]; then
-        bash ${install_path}/${conda_name} -b -p ${HOME}/miniconda3
-    fi
+wget https://repo.anaconda.com/miniconda/${conda_name} -P ${install_path}
+if [ $? -eq 0 ]; then
+    bash ${install_path}/${conda_name} -b -p ${HOME}/miniconda3
 fi
 
 echo "${conda_name} installed with silent mode."
--- softwares/internet/submodules/miniconda/miniconda.sh
+++ softwares/internet/submodules/miniconda/miniconda.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-. $(pwd)/softwares/internet/submodules/miniconda/install.sh
-
-if [ $? -eq 0 ]; then
-    . $(pwd)/softwares/internet/submodules/miniconda/change_sources.sh
+if [ ! -d ${HOME}/miniconda3 ]; then
+    . $(pwd)/softwares/internet/submodules/miniconda/install.sh
+    if [ $? -eq 0 ]; then
+        . $(pwd)/softwares/internet/submodules/miniconda/change_sources.sh
+    fi
 fi
